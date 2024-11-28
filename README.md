25d272c6537e94480839769a6a354953aae8d0b6
jayson <jays.labarrete@gmail.com>
2024-11-28 15:52:04 +1100

    finalise k6 tests

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a performance testing exercise using postman collections
 API source: https://petstore.swagger.io/#/
 tool used for performance: https://grafana.com/docs/k6/latest/get-started/running-k6/
+https://fakerjs.dev/api/number.html
+BLOG that was referenced: https://medium.com/10ms-technology-blog/load-testing-made-easy-with-k6-using-faker-library-and-csv-files-c997d48fb6e2
--- generateData.js
+++ generateData.js
@@ -1,17 +1,17 @@
-import faker from "https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js";
+import { faker } from "https://esm.sh/@faker-js/faker@v9.2.0";
 
 export const generateData = () => ({
-  id: faker.string.uuid(),
+  id: faker.number.int({ min: 100, max: 999 }),
   category: {
-    id: faker.string.uuid(),
+    id: faker.number.int({ min: 100, max: 999 }),
     name: faker.animal.type(),
   },
   name: faker.animal.type(),
   photoUrls: ["string"],
   tags: [
     {
-      id: faker.string.uuid(),
-      name: faker.address.city(),
+      id: faker.number.int({ min: 100, max: 999 }),
+      name: faker.location.city(),
     },
   ],
   status: "available",
